e409e4fa0be126496c5dec763c2bfc640441255b
Felipe Marinho <felipmarinho@gmail.com>
2020-03-01 05:21:12 -0300

    global object

---
+++ capitulo02/demo10-objetos-globais.js
@@ -0,0 +1,8 @@
+console.log('keys', Object.keys({name: 'Felipe'}));
+console.log('values', Object.values({name: 'Felipe'}));
+console.log('now', Date.now());
+console.log('random', Math.random());
+
+// variable global
+console.log('global', global);
+
